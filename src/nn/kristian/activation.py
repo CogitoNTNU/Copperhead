14cc6e1960a702d6f365e6ec7d0bc61d8c36b737
fredalex6 <frederik.a.adlandsvik@hotmail.com>
2026-09-22 16:03:58 +0200

    feat: add ReLU module

---
+++ src/nn/kristian/activation.py
@@ -0,0 +1,21 @@
+import numpy as np
+from src.nn.kristian.module import Module
+
+
+class ReLU(Module):
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        """x: (batch, n) -> (batch, n). Save mask for backward."""
+
+        sigma = np.maximum(0, x)
+        self.mask = sigma > 0  # boolean array
+
+        return sigma
+
+    def backward(self, dout):
+        return dout * self.mask
+
+    def parameters(self):
+        return []
